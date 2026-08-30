5f1fd9feb352a8b3e75529cbee3e1659e4a06b88
goksel26 <gokselaktas84@gmail.com>
2026-08-30 03:56:04 +0300

    1.4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    viteSingleFile(),
+  ],
   build: {
     outDir: 'dist',
+    emptyOutDir: true
   }
 })
